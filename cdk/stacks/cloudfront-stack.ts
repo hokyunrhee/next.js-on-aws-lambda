a35506fc015da749c038b9c92c0b4376c86eed2b
hokyunrhee <hokyun.rhee@gmail.com>
2023-09-02 14:16:09 +0900

    infra: CloudFront originRequestPolicy 추가

--- cdk/stacks/cloudfront-stack.ts
+++ cdk/stacks/cloudfront-stack.ts
@@ -1,6 +1,14 @@
 import { Stack, StackProps } from "aws-cdk-lib"
 import { RestApi } from "aws-cdk-lib/aws-apigateway"
-import { CachePolicy, Distribution, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront"
+import {
+  AllowedMethods,
+  CachePolicy,
+  Distribution,
+  OriginRequestCookieBehavior,
+  OriginRequestPolicy,
+  OriginRequestQueryStringBehavior,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront"
 import { RestApiOrigin, S3Origin } from "aws-cdk-lib/aws-cloudfront-origins"
 import { Bucket } from "aws-cdk-lib/aws-s3"
 import { Construct } from "constructs"
@@ -16,20 +24,29 @@ export class CloudfrontStack extends Stack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props)
 
+    const originRequestPolicy = new OriginRequestPolicy(this, `${prefix}-origin-request-policy`, {
+      cookieBehavior: OriginRequestCookieBehavior.all(),
+      queryStringBehavior: OriginRequestQueryStringBehavior.all(),
+    })
+
     new Distribution(this, `${prefix}-distribution`, {
       defaultBehavior: {
         origin: new RestApiOrigin(props.restApi),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: AllowedMethods.ALLOW_ALL,
         cachePolicy: CachePolicy.CACHING_DISABLED,
+        originRequestPolicy,
       },
       additionalBehaviors: {
         "_next/static/*": {
           origin: new S3Origin(props.bucket),
           viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+          allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
         },
         "static/*": {
           origin: new S3Origin(props.bucket),
           viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+          allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
         },
       },
     })
